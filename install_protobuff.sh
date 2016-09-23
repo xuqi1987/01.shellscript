8b52307b098f79f6ccdbc9ca59f389a31fe95dda
Xu, Qi <xuqi.jx@gmail.com>
2016-09-23 00:19:02 +0000

    add protobuf

---
+++ install_protobuff.sh
@@ -0,0 +1,10 @@
+cd ..
+sudo apt-get install autoconf automake libtool curl make g++ unzip
+git clone https://github.com/google/protobuf.git
+cd protobuf
+./autogen.sh 
+./configure
+make -j4
+make check
+sudo make install
+sudo ldconfig
