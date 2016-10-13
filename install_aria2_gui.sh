2983155ed8bdb0849d0eaa18e72fcbc28a807301
x2020 <x2020@example.com>
2016-10-13 03:37:26 -0400

    add gui script

---
+++ install_aria2_gui.sh
@@ -0,0 +1,4 @@
+cd ..
+git clone git@github.com:ziahamza/webui-aria2.git
+
+mv webui-aria2 03.ubuntu_Server/aria2 
---
+++ install_nginx.sh
@@ -0,0 +1,14 @@
+sudp apt-get update 
+sudo apt-key add nginx_signing.key
+
+sudo apt-get install nginx -y
+
+cd ..
+if [ ! -d  03.ubuntu_Server ]; then
+	git clone  git@github.com:xuqi1987/03.ubuntu_Server.git
+fi;
+cd 03.ubuntu_Server/nginx/
+ln -s /etc/nginx/sites-enabled/default ./
+ln -s /var/www/html ./
+nginx -s reload
+
