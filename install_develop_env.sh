888b9c8341b99c19a506f78276a89d78f176aef4
xuqi1987 <xuqi.jx@gmail.com>
2018-04-25 20:55:53 +0800

    add evn script

---
+++ install_develop_env.sh
@@ -0,0 +1,88 @@
+sudo apt-get install synaptic
+sudo apt-get install apt-file -y
+sudo apt-file update
+sudo apt-get install nautilus-open-terminal -y
+
+sudo apt-get install chromium-browser -y
+sudo apt-get install chromium-codecs-ffmpeg-extra
+sudo apt-get install gimp -y
+sudo apt-get install vim -y
+sudo apt-get install ssh -y
+sudo apt-get install dos2unix -y
+sudo apt-get install xrdp -y
+
+sudo apt-get install build-essential -y
+sudo apt-get install git -y
+sudo apt-get install subversion -y
+sudo apt-get install bison -y
+sudo apt-get install gperf -y
+sudo apt-get install ccache -y
+sudo apt-get install gitk -y
+sudo apt-get install meld -y
+sudo apt-get install eclipse eclipse-cdt -y
+sudo apt-get install dh-autoreconf -y
+sudo apt-get install cmake -y
+
+sudo apt-get install libpulse-dev -y
+sudo apt-get install libdirectfb-dev -y
+sudo apt-get install libpango1.0-dev -y
+sudo apt-get install libdbus-1-dev -y
+sudo apt-get install libasound2-dev -y
+sudo apt-get install libesd0-dev -y
+sudo apt-get install libncurses5-dev -y
+sudo apt-get install libicu-dev -y
+sudo apt-get install libxml2-dev -y
+sudo apt-get install libsqlite3-dev -y
+sudo apt-get install libegl1-mesa-dev -y
+sudo apt-get install libgles2-mesa-dev -y
+sudo apt-get install libevent-dev -y
+sudo apt-get install libxcursor-dev -y
+sudo apt-get install libgstreamer0.10-dev -y
+sudo apt-get install libgstreamer-plugins-base0.10-dev -y
+sudo apt-get install gstreamer0.10-plugins-good -y
+sudo apt-get install gstreamer0.10-plugins-ugly -y
+sudo apt-get install gstreamer0.10-plugins-bad -y
+sudo apt-get install gstreamer0.10-plugins-bad-multiverse -y
+sudo apt-get install gstreamer0.10-fluendo-mp3 -y
+sudo apt-get install gstreamer0.10-ffmpeg -y
+sudo apt-get install gstreamer0.10-libav -y
+sudo apt-get install ffmpeg -y
+sudo apt-get install pyside-tools -y
+sudo apt-get install python-m2crypto -y
+sudo apt-get install python-lxml -y
+sudo apt-get install lib32ncurses5-dev -y
+sudo apt-get install lib32z1-dev -y
+sudo apt-get install ia32-libs -y
+sudo apt-get install libc-dev-i386 -y
+sudo apt-get install g++-multilib -y
+sudo apt-get install libc6-dev-i386 -y
+
+sudo apt-get install gnupg flex  -y
+sudo apt-get install zip -y
+sudo apt-get install curl -y
+sudo apt-get install libc6-dev -y
+sudo apt-get install libncurses5-dev:i386 -y
+sudo apt-get install x11proto-core-dev -y
+sudo apt-get install libx11-dev:i386 -y
+sudo apt-get install libreadline6-dev:i386 -y
+sudo apt-get install libglapi-mesa:i386 -y
+sudo apt-get install libgl1-mesa-glx:i386 -y
+sudo apt-get install libgl1-mesa-dev -y
+sudo apt-get install mingw32 -y
+sudo apt-get install tofrodos -y
+sudo apt-get install python-markdown -y
+sudo apt-get install xsltproc -y
+sudo apt-get install zlib1g-dev:i386 -y 
+sudo apt-get install libxml2-utils -y
+sudo apt-get install libc-dev:i386 -y
+sudo apt-get install g++-4.6 gcc-4.6 -y
+sudo apt-get install g++-4.6-multilib -y
+sudo apt-get install xserver-xorg -y
+sudo apt-get install xserver-common -y
+sudo apt-get install rabbitvcs-nautilus rabbitvcs-gedit -y
+sudo apt-get install g++-4.7-multilib -y
+sudo apt-get install eclipse_cdt -y
+sudo apt-get install android-tools-adb -y
+sudo apt-get install libtiff4 -y
+
+echo "Done."
