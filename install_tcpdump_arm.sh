fd8abe2b42479b0bb8054c456822ee7d505e95a3
XuQi <xuqi.jx@gmail.com>
2018-11-02 12:18:03 +0800

    add

---
+++ install_tcpdump_arm.sh
@@ -0,0 +1,37 @@
+export Source="$(pwd)/source"
+export Compressed="$(pwd)/compressed"
+export Install="$(pwd)/install"
+
+if [ ! -d "$Install" ]; then
+
+mkdir -p $Source
+mkdir -p $Install
+mkdir -p $Compressed
+
+
+# donwload code
+cd $Compressed
+
+wget http://www.tcpdump.org/release/tcpdump-4.9.2.tar.gz
+wget http://www.tcpdump.org/release/libpcap-1.9.0.tar.gz
+
+tar zxvf tcpdump-4.9.2.tar.gz  -C  $Source
+tar zxvf libpcap-1.9.0.tar.gz  -C  $Source
+
+cd $Source/libpcap-1.9.0
+CC=arm-none-linux-gnueabi-gcc  ./configure --host=arm-linux --with-pcap=linux --prefix=$Install/
+make 
+make install
+
+cd $Source/tcpdump-4.9.2
+
+CC=arm-none-linux-gnueabi-gcc ./configure --host=arm-linux --with-pcap=linux --prefix=$Install/
+make 
+make install
+
+cd $Install
+tar -zcf ../tcpdump.tar.gz ./bin ./lib ./sbin
+
+fi
+
+
